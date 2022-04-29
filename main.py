ded032ea58081b5be073f7be54b2fe0bf91a9a73
edwardinchina <edwardinchina@outlook.com>
2022-04-29 23:36:30 +0000

    part 2

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello AWS World!")
+import logging
+from logging.config import dictConfig
+from log_config import log_config # this is your local file
+
+logging.config.dictConfig(log_config)
+
+logger = logging.getLogger("reddit-broker-bot") # name of logger in loggers dict
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    logger.info('/')
+    return {"message": "Hello World"}
+
+
+@app.get("/health")
+async def health():
+    logger.info('/health')
+    return {"message": "Healthy"}
+
