6f4f503111b444459724e3f1333f3763138e1beb
Edward Mitchell <edwardmitchell.data@gmail.com>
2022-04-29 20:35:23 -0400

    part 3

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import logging
 from logging.config import dictConfig
 from log_config import log_config # this is your local file
 
+from pydantic import BaseModel
+
+from transformers import pipeline
+
 logging.config.dictConfig(log_config)
 
 logger = logging.getLogger("reddit-broker-bot") # name of logger in loggers dict
@@ -21,3 +25,18 @@ async def health():
     logger.info('/health')
     return {"message": "Healthy"}
 
+class PredictionRequest(BaseModel):
+    query_string: str
+
+
+@app.on_event("startup")
+def load_model():
+    global sentiment_model
+    sentiment_model = pipeline("sentiment-analysis")
+
+@app.post("/my-endpoint")
+def my_endpoint(request: PredictionRequest):
+    sentiment_query_sentence = request.query_string
+    sentiment = sentiment_model(sentiment_query_sentence)
+    print(f"Sentiment test: {sentiment_query_sentence} = {sentiment}")
+    return {"sentiment": sentiment}
